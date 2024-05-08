b6d5d998985879d50a2dc4aafaed6ee6f17ce4d2
Trey Miller <treymillersemail@gmail.com>
2024-05-07 20:46:04 -0700

    Added speaking text

--- src/psyche/components/PopUps/MissionIntroPopup.jsx
+++ src/psyche/components/PopUps/MissionIntroPopup.jsx
@@ -1,27 +1,11 @@
 import React from 'react';
 import robotImage from '/assets/psyche_bot.png';
-
-// function MissionIntroPopup({ title, message, onClose, onNext, isLast }) {
-//   return (
-//     <div className="mission-intro-popup">
-
-//       <div className="character-image">
-//         <img src={robotImage} alt="Robot Character" />
-//       </div>
-//       <div className="text-content">
-//         <h1>{title}</h1>
-//         <p>{message}</p>
-//         {isLast ? (
-//           <button onClick={onClose}>Finish</button>
-//         ) : (
-//           <button onClick={onNext}>Next</button>
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
+ import useTypingText from '../../utils/useTypingEffect';
 
 function MissionIntroPopup({ title, message, onClose, onNext, isLast }) {
+  const typedMessage = useTypingText(message, 23); //speed
+
+
   return (
     <div className="mission-intro-popup">
       <div className="popup-content">
@@ -30,7 +14,7 @@ function MissionIntroPopup({ title, message, onClose, onNext, isLast }) {
         </div>
         <div className="text-content">
           <h1>{title}</h1>
-          <p>{message}</p>
+          <p>{typedMessage}</p>
           {isLast ? (
             <button onClick={onClose} >Finish</button>
           ) : (
