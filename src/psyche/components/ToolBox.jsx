30063c678e5741bf6df87fe612222ede19e005c9
Atracytr <96859183+Atracytr@users.noreply.github.com>
2024-05-21 22:49:45 -0700

    Popup tools intergrate w bot (#31)
    * temp
    
    * responsive

--- src/psyche/components/ToolBox.jsx
+++ src/psyche/components/ToolBox.jsx
@@ -1,5 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { GlobalStateContext } from '../utils/useContext';
+
 import './ToolBox.css';
+import '../style.css'
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import InfoContent from './PopUps/InfoContent';
+
+
 
 const images = [
   'public/assets/multiSpec_Imager.png',
@@ -16,11 +24,41 @@ const imageTitles = [
 const ToolBox = () => {
   const [visibleColumns, setVisibleColumns] = useState(images.map(() => true));
 
+  const { currentFactIndex, setCurrentFactIndex, factList, toolList,currentToolIndex,setCurrentToolIndex, setIsModalOpen } = useContext(GlobalStateContext);
+
   const handleClick = index => {
     setVisibleColumns(visibleColumns.map((isVisible, columnIndex) =>
       columnIndex === index ? !isVisible : isVisible
     ));
+    setCurrentToolIndex(index);
   };
+  
+  const handleClose = () => {
+		setCurrentToolIndex(null);
+		//setIsModalOpen(false);
+	}
+  
+  const modalStyle = {
+		position: 'fixed',
+      top: '50%',
+      left: '50%',
+      
+      maxHeight: '80vh',
+      //maxWidth: '65vw',
+			width:'70vw',
+      //height:'50vh',
+      
+      transform: 'translate(-50%, -50%)',
+      backgroundColor: 'rgba(255, 255, 255, 1)',
+      padding: '3vh',
+      borderRadius: '15px',
+      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+      zIndex: 1000,
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'top',
+      alignItems: 'center'
+	};
 
   // Check if all images are invisible
   const allInvisible = visibleColumns.every(isVisible => !isVisible);
@@ -31,19 +69,37 @@ const ToolBox = () => {
   }
 
   return (
-    <div className="gallery-container">
-      {images.map((image, index) => (
-        <div key={index} className="gallery-column" onClick={() => handleClick(index)}
-             style={{
-               visibility: visibleColumns[index] ? 'visible' : 'hidden',
-               opacity: visibleColumns[index] ? 1 : 0,
-               transition: 'opacity 0.5s, visibility 0.5s'
-             }}>
-          <img src={image} alt={imageTitles[index]} className="gallery-image" />
-          <div className="image-title">{imageTitles[index]}</div>
-        </div>
+    <>
+    <Box className="gallery-container">
+      {toolList.map((tool, index) => (
+        <>
+          <Box key={index} onClick={() => handleClick(index)}
+              style={{
+                visibility: visibleColumns[index] ? 'visible' : 'hidden',
+                opacity: visibleColumns[index] ? 1 : 0,
+                transition: 'opacity 0.5s, visibility 0.5s',
+                margin:'1vw',
+              }}>
+                <Box>
+                {tool.icon}
+                </Box>
+                <Box className='text'>
+                {tool.title}
+                </Box>
+          </Box>
+         </>
       ))}
-    </div>
+        
+
+    </Box>
+    {currentToolIndex!== null &&
+    <Modal open={currentToolIndex != null}
+    onClose={handleClose}>
+     <Box sx={modalStyle}>
+     <InfoContent index={currentToolIndex} array={toolList} />
+     </Box>
+    </Modal>}
+    </>
   );
 };
 
